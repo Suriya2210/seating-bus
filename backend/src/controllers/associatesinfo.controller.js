812a1f0c0628178a059c3439333705d225dce897
GK <jaivigneshgk.g@esko.com>
2024-02-20 11:42:24 +0530

    Added the get-bookings route

--- backend/src/controllers/associatesinfo.controller.js
+++ backend/src/controllers/associatesinfo.controller.js
@@ -1,5 +1,6 @@
 const {Sequelize} = require('sequelize');
 const User = require('../models/user');
+const BookSeat = require('../models/seat_booking');
 exports.getAssociates = (req, res, next)=>{
     User.findAll({where:{
         manager_id:req.params.manager_id
@@ -17,6 +18,20 @@ exports.getAssociates = (req, res, next)=>{
 exports.getAssociate = (req,res,next)=>{
     User.findOne({where:{
         associate_id:req.params.associate_id
+    }}).then(user=>{
+        console.log(user);
+        res.status(200).send({
+            stats:"success",
+            data:{
+                user:user
+            }
+        });
+    })
+}
+
+exports.getBookingHistory = (req,res,next)=>{
+    BookSeat.findAll({where:{
+        seat_booked_by:req.params.associate_id
     }}).then(user=>{
         console.log(user);
         res.status(200).send({
--- backend/src/routes/associatesinfo.route.js
+++ backend/src/routes/associatesinfo.route.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const associatesinfo_controller = require('../controllers/associatesinfo.controller');
 router.route('/get-associates/:manager_id').get(associatesinfo_controller.getAssociates);
 router.route('/get-associate/:associate_id').get(associatesinfo_controller.getAssociate);
+router.route('/get-bookings/:associate_id').get(associatesinfo_controller.getBookingHistory);
 module.exports = router;
